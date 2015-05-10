2b9765abd5395e0ba14d0b3300190a508040e267
Novotnik, Petr <petr.novotnik@firma.seznam.cz>
2015-05-10 13:01:16 +0200

    Return early when reading file fails

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,19 @@
 
 extern crate regex;
 extern crate getopts;
+
+#[macro_use(try_read_lines)]
 extern crate lines;
 
 use std::env;
 use std::fs::File;
-use std::io::{BufReader, Write, stderr, stdout};
+use std::io::{Write, stderr, stdout};
 use std::io::Result as IoResult;
 
 use getopts::Options;
 
 use regex::Regex;
-use lines::linemapper;
+use lines::linereader::LineReader;
 
 fn main() {
     let (cfg, files) = match Config::from_cmdline(env::args().collect()) {
@@ -78,33 +80,33 @@ impl Config {
 const NEWLINE: &'static [u8] = &[b'\n'];
 const SPACE: &'static [u8] = &[b' '];
 
-// ~ XXX use linereader api and return the io error instead of
-// handling it here in this function
 fn process_arg(cfg: &Config, filename: &str) -> IoResult<()> {
     let f = try!(File::open(filename));
-    let r = BufReader::new(f);
 
     let mut block = Vec::with_capacity(1024);
-    let mut stdout = stdout();
+    let stdout = stdout();
+    let mut stdout = stdout.lock();
 
     let mut line_no = 0usize;
     let mut collecting_block = false;
     let mut output_produced = false;
-    linemapper::map_lines(r, |line| {
+
+    // ~ Collect blocks of lines beloning together and spill them out.
+    // ~ A block is defined by two or multiple successive lines of
+    // which on the first matches the configured regular expression.
+    try_read_lines!(line in LineReader::new(f), {
         line_no += 1;
         if cfg.line_regex.is_match(&String::from_utf8_lossy(line)) {
             if collecting_block {
                 // ~ abort as soon as possible if the write didn't
                 // succeed e.g. broken pipe
-                if let Err(e) = stdout.write_all(&block) {
-                    let _ = write!(&mut stderr(), "{}", e);
-                    env::set_exit_status(1);
-                    return false;
-                }
+                try!(stdout.write_all(&block));
                 output_produced = true;
                 collecting_block = false;
             }
-            block.clear();
+
+            // block.clear();
+            unsafe { block.set_len(0); }
 
             if output_produced {
                 let _ = block.write_all(NEWLINE);
@@ -123,7 +125,7 @@ fn process_arg(cfg: &Config, filename: &str) -> IoResult<()> {
         if cfg.print_line_numbers {
             let _ = write!(&mut block, "{:7} ", line_no);
         }
-        let _ = block.write(line);
-        true
-    })
+        let _ = block.write_all(line);
+    });
+    Ok(())
 }
